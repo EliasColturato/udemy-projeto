a608d5cc4879c4e5c691eb620fc8330d90547869
Elias Colturato da Silva <91541899+EliasColturato@users.noreply.github.com>
2022-10-12 17:36:35 -0300

    Finalização do projeto
    	A finalização do projeto foi dada pela criação da página de
    	favoritos, assim como foi adicionar a biblioca "react-toastify"
    	para estilização dos alertas na aplicação

--- src/pages/Filmes/filme.js
+++ src/pages/Filmes/filme.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 import './styleFilme.css';
+import { toast } from 'react-toastify';
 
 function Filme() {
   const { id } = useParams();
+  const navigation = useNavigate();
+
   const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
+
   const img_url = 'https://image.tmdb.org/t/p/w500';
 
   useEffect(() => {
@@ -24,15 +28,32 @@ function Filme() {
         })
         .catch(() => {
           console.log('Filme não encontrado');
+          navigation('/', { replace: true });
+          return;
         });
     }
 
     loadFilme();
+  }, [navigation, id]);
 
-    return () => {
-      console.log('COMPONENTE FOI DESMONTADO');
-    };
-  });
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem('@primeflix');
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some(
+      filmesSalvos => filmesSalvos.id === filme.id
+    ); //verifica se dentro da nossa array se já existe algum item com o mesmo id que o usuario está tentando usar
+
+    if (hasFilme) {
+      toast.warn('Esse filme já está na sua lista');
+      return;
+    } //if para caso o filme já exista na lista
+
+    filmesSalvos.push(filme); //caso o filme não exista na lista o push é responsável por adicionar o item a nossa lista
+    localStorage.setItem('@primeflix', JSON.stringify(filmesSalvos));
+    toast.success('Filme salvo com sucesso');
+  }
 
   if (loading) {
     return (
@@ -53,9 +74,15 @@ function Filme() {
       <span>{filme.overview}</span>
       <strong>{filme.vote_average} / 10</strong>
       <div className="area-button">
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
-          <a href="#">Trailer</a>
+          <a
+            href={`https://youtube.com/results?search_query=${filme.title} trailer`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Trailer
+          </a>
         </button>
       </div>
     </div>
